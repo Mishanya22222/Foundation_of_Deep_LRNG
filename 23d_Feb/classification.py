618aec9587044cf87ee51846f344f8ed3ddc673f
Mishanya22222 <mpolishchuk930@gmail.com>
2026-02-23 15:02:13 +0000

    classification

---
+++ 23d_Feb/classification.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import torch 
+import torch.nn as nn
+import torch.optim as optim
+
+torch.manual_seed(42)
+
+data = pd.read_csv("data.csv") 
+data["Diagnosis"] = data["Diagnosis"].map({"Benign":0, "Malignant":1}) # changing the string target as an binry integers
+
+features = torch.tensor(data.drop("Diagnosis", axis = 1).to_numpy()).float()
+target = torch.tensor(data["Diagnosis"].to_numpy()).float().reshape(-1,1)
+
+fm = features.mean(axis = 0, keepdim = True).reshape(-1,1) # look up the difference of axis = 0 and axis = 1
+fs = features.std(axis = 0,keepdim = True).reshape(-1,1)
+tm = target.mean(axis = 0,keepdim = True).reshape(-1,1)
+ts = target.std(axis = 0,keepdim = True).reshape(-1,1)
+
+X = (features - fm)/fs
+Y = (target- tm)/ts
+
+model = nn.Linear(1,1) # we don't have to put the sigmoid fucntion here
+criterian = nn.BCEWithLogitsLoss() # because it's included here
+optimizer = optim.SGD(model.parameters(), lr = .1)
+
+epochs = 250
+
+for epoch in range(epochs):
+    y_hat = model(X)
+    loss = criterian(y_hat, Y)
+    loss.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+    print(loss)

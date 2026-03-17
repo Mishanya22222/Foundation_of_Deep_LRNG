e97d12e42458f0b06c7b60387383b444afbb5cc0
Mishanya22222 <mpolishchuk930@gmail.com>
2026-03-17 01:57:27 +0000

    exam prpe

--- 27th_Feb/train1.py
+++ 27th_Feb/train1.py
@@ -5,27 +5,23 @@ import pandas as pd
 import numpy as np
 from export import export_model
 
-df = pd.read_csv("data1.csv")
+data = pd.read_csv("data.csv")
 
-X = torch.tensor(df.drop("y", axis = 1).values).float() # instead of to_numpy we can also use values, and the difference is that values are list
-Y = torch.tensor(df["y"].values).float().reshape(-1,1)
+X = torch.tensor(data.drop("y", axis = 1).values, dtype = torch.float32)
+Y = torch.tensor(data['y'].values, dtype = torch.float32).reshape(-1, 1)
 
-model = nn.Sequential(
-    nn.Linear(2,4),
-    nn.ReLU(),
-    nn.Linear(4,1)) # adding a hidden layer
-
-criterion = nn.BCEWithLogitsLoss() # we have the sigmoid here
+model = nn.Linear(2,1)
+crtiterion = nn.BCEWithLogitsLoss()
 optimizer = optim.Adam(model.parameters(), lr = 0.1)
 
-epochs = 1000
+epochs = 250
 
 for epoch in range(epochs):
     y_hat = model(X)
-    loss = criterion(y_hat,Y)
+    loss = crtiterion(y_hat, Y)
     loss.backward()
     optimizer.step()
     optimizer.zero_grad()
     print(loss)
 
-export_model(model, "model1.json") # name of the model ane the name we want to give 
+export_model(model, "model.json")

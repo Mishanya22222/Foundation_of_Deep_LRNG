91f20e96d3785da3be3cb512c1c82b19c725bb99
Mishanya22222 <mpolishchuk930@gmail.com>
2026-03-23 13:53:45 +0000

    training the image NN

--- 20th_Mar/mnist.py
+++ 20th_Mar/mnist.py
@@ -1,8 +1,13 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 from grid import save_image_grid
+import torch.nn as nn
+import torch
+import torch.optim as optim 
 import matplotlib
 import math
+
+torch.manual_seed(1)
 
 dataset = datasets.MNIST(
     root = "./data", # makes another folder called data
@@ -11,17 +16,35 @@ dataset = datasets.MNIST(
     transform = transforms.ToTensor() # comes in as tensors 
 )
 
-# image, label = dataset[0]
-# image.save("image.png")
-
-
-
 loader = DataLoader(
     dataset, 
-    batch_size = 10
+    batch_size = 64,
+    shuffle = True # send all of the images through for the first epoch, than shiffle and then send again for the second epoch, and so on.
 )
 
-for i,(images, labels) in enumerate(loader):
-    save_image_grid(images)
-    if i ==9:
-        break
+model = nn.Sequential(
+    nn.Flatten(),
+    nn.Linear(784, 128),  # input
+    nn.ReLU(),
+    nn.Linear(128, 64),
+    nn.ReLU(),
+    nn.Linear(64,10) # output
+)
+
+criterion = nn.CrossEntropyLoss() # loss function for classification problems, which has the softmax and will produce the probability
+optimizer = optim.Adam(model.parameters(), lr= 0.001)
+
+epochs = 10
+
+# how to print only last loose withn batch
+for epoch in range(epochs):
+    for images, labels in loader:
+        y_hat = model(images)
+        loss = criterion(y_hat, labels)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+    print(f"epoch {epoch+1}/{epochs} last batch loss = {loss.item():.4f}")
+
+
+

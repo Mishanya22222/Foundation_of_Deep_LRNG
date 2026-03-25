80948a879f33b1704629ff96ca0b29e2d79f89b0
Mishanya22222 <mpolishchuk930@gmail.com>
2026-03-25 13:49:52 +0000

    finished training of the image recognition

--- 20th_Mar/mnist.py
+++ 20th_Mar/mnist.py
@@ -4,8 +4,6 @@ from grid import save_image_grid
 import torch.nn as nn
 import torch
 import torch.optim as optim 
-import matplotlib
-import math
 
 torch.manual_seed(1)
 
@@ -16,10 +14,23 @@ dataset = datasets.MNIST(
     transform = transforms.ToTensor() # comes in as tensors 
 )
 
+test_dataset = datasets.MNIST(
+    root = "./data",
+    train = False,
+    download= True,
+    transform = transforms.ToTensor()
+)
+
 loader = DataLoader(
     dataset, 
     batch_size = 64,
     shuffle = True # send all of the images through for the first epoch, than shiffle and then send again for the second epoch, and so on.
+)
+
+test_loader = DataLoader(
+    test_dataset,
+    batch_size = 10000,
+    shuffle = False
 )
 
 model = nn.Sequential(
@@ -38,13 +49,19 @@ epochs = 10
 
 # how to print only last loose withn batch
 for epoch in range(epochs):
+    total_loss = 0
+    correct = 0
+    total = 0
     for images, labels in loader:
+        optimizer.zero_grad()
         y_hat = model(images)
         loss = criterion(y_hat, labels)
         loss.backward()
         optimizer.step()
-        optimizer.zero_grad()
-    print(f"epoch {epoch+1}/{epochs} last batch loss = {loss.item():.4f}")
-
+        total_loss += loss.item()
+        total += labels.size(0) # number of samples in the batch
+        correct += (y_hat.argmax(1) == labels).sum().item() # number of correct predictions in the batch
+        
+    print(f"Epoch: {epoch}, Loss: {total_loss/len(loader)}, Accuracy: {correct/total}") # batches in an epoch, so we are getting the average loss per batch
 
 
